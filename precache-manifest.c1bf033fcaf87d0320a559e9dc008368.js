2b65dc932deba1639bcac2ac5851f4dae04b6edc
Yunwen Zheng <ynn.zheng@gmail.com>
2020-04-10 21:54:35 -0400

    Updates

---
+++ precache-manifest.c1bf033fcaf87d0320a559e9dc008368.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9857b2dcdca80f05ae66846798061433",
+    "url": "/react-animal-crossing/index.html"
+  },
+  {
+    "revision": "203e1788d989ed88c258",
+    "url": "/react-animal-crossing/static/css/main.7ea50ef3.chunk.css"
+  },
+  {
+    "revision": "b52642db158c3b93fc1d",
+    "url": "/react-animal-crossing/static/js/2.bb666597.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-animal-crossing/static/js/2.bb666597.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "203e1788d989ed88c258",
+    "url": "/react-animal-crossing/static/js/main.416ebae9.chunk.js"
+  },
+  {
+    "revision": "f55b5d226e8f4c2387a7",
+    "url": "/react-animal-crossing/static/js/runtime-main.c8dd2b61.js"
+  },
+  {
+    "revision": "47be2894cbddbdb9c8190254d72432c2",
+    "url": "/react-animal-crossing/static/media/ac_logo.47be2894.png"
+  },
+  {
+    "revision": "0ef25a97e25e0ac4d56aa8ca8ac4a0c2",
+    "url": "/react-animal-crossing/static/media/background.0ef25a97.png"
+  },
+  {
+    "revision": "ce5a31af654c05e2a5d4c79ac6c00f5f",
+    "url": "/react-animal-crossing/static/media/leaf.ce5a31af.svg"
+  },
+  {
+    "revision": "9b995b32b3bb259f39f3d6915d38c951",
+    "url": "/react-animal-crossing/static/media/passport-background.9b995b32.png"
+  }
+]);
